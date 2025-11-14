35f394355852d62f85d2de03fee9492347fc8e1c
AnnaMcCormick <anna.mccormick@kainos.com>
2025-11-14 16:56:50 +0000

    Key vault RG (#1)
    * Key vault RG
    
    Key vault and key vault RG. Pipeline has been created for automatic deployment.
    
    * Possible fix?
    
    * Fixed?

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-mgmt"
+    storage_account_name = "aistatemgmt"
+    container_name       = "terraform-tfstate-ai"
+    key                  = "team1.sr.dev.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "UK South"
+}
+
