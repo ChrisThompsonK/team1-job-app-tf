e5769b8495991114e084b80f9189e2f71ef4371d
AnnaMcCormick <anna.mccormick@kainos.com>
2025-11-18 15:49:04 +0000

    Made a new env for Ollie and Carter (#6)
    * Made a new env for Ollie and Carter
    
    This is hopefully all good
    
    * created shared infra resource group
    
    Ollie approves :)
    
    * Fixed
    
    ---------
    
    Co-authored-by: ollie-small <ollie.small@kainos.com>

--- main.tf
+++ main.tf
@@ -25,8 +25,8 @@ provider "azurerm" {
   }
 }
 
-resource "azurerm_resource_group" "team1-job-app-key-rg" {
-  name     = "team1-job-app-key-rg"
-  location = "UK South"
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
 }
-
