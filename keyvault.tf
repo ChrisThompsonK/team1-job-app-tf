e9f7c65f7514c2ea5c7f5832695d9e966e8b548b
AnnaMcCormick <anna.mccormick@kainos.com>
2025-11-18 15:35:26 +0000

    Fixed

--- keyvault.tf
+++ keyvault.tf
@@ -1,11 +1,11 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "main" {
-  name                = var.key_vault_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
+  name                      = var.key_vault_name
+  location                  = azurerm_resource_group.main.location
+  resource_group_name       = azurerm_resource_group.main.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
   enable_rbac_authorization = true
 
   tags = var.tags
